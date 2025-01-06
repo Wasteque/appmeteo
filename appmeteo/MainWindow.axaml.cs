0e07c2e1bf0dd529d5f43c8ed97b48d7d00f7042
unknown <ethan.dufourcanellas@ynov.com>
2025-01-06 02:00:11 +0100

    AppMeteo

---
+++ appmeteo/MainWindow.axaml.cs
@@ -0,0 +1,128 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Newtonsoft.Json.Linq;
+using Avalonia.Media;
+using Avalonia.Threading;
+
+namespace appmeteo
+{
+    public partial class MainWindow : Window
+    {
+        private TextBox cityTextBox;
+        private Button searchButton;
+        private TextBlock weatherResultText;
+        private TabControl tabs;
+
+        
+        private GradientStop gradientStop1;
+        private GradientStop gradientStop2;
+        private LinearGradientBrush backgroundGradientBrush;
+        private DispatcherTimer animationTimer;
+        private double hue1 = 0;
+        private double hue2 = 100;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            cityTextBox = this.FindControl<TextBox>("CityTextBox");
+            searchButton = this.FindControl<Button>("SearchButton");
+            weatherResultText = this.FindControl<TextBlock>("WeatherResultText");
+            tabs = this.FindControl<TabControl>("Tabs");
+
+            searchButton.Click += async (sender, e) => await SearchWeatherAsync();
+
+           backgroundGradientBrush = (LinearGradientBrush)this.Resources["GradientBrush"];
+            gradientStop1 = backgroundGradientBrush.GradientStops[0];
+            gradientStop2 = backgroundGradientBrush.GradientStops[1];
+
+            animationTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(25)
+            };
+            animationTimer.Tick += AnimateGradient;
+            animationTimer.Start();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+         
+        private void AnimateGradient(object sender, EventArgs e)
+        {
+            backgroundGradientBrush.EndPoint = new RelativePoint((double)this.ClientSize.Width, (double)this.ClientSize.Height, RelativeUnit.Absolute);
+            gradientStop1.Color = ColorFromHue(hue1);
+            gradientStop2.Color = ColorFromHue(hue2);
+            gradientStop2.Offset = Math.Max((double)this.ClientSize.Width, (double)this.ClientSize.Height);
+            hue1 = (hue1 + 1) % 360;
+            hue2 = (hue2 + 1) % 360;
+
+            backgroundGradientBrush.GradientStops[0] = gradientStop1;
+            backgroundGradientBrush.GradientStops[1] = gradientStop2;
+        }
+
+        private Color ColorFromHue(double hue)
+        {
+            hue = hue % 360;
+            if (hue < 60)
+                return Color.FromRgb((byte)(255), (byte)(hue * 255 / 60), 0);
+            else if (hue < 120)
+                return Color.FromRgb((byte)((120 - hue) * 255 / 60), 255, 0);
+            else if (hue < 180)
+                return Color.FromRgb(0, 255, (byte)((hue - 120) * 255 / 60));
+            else if (hue < 240)
+                return Color.FromRgb(0, (byte)((240 - hue) * 255 / 60), 255);
+            else if (hue < 300)
+                return Color.FromRgb((byte)((hue - 240) * 255 / 60), 0, 255);
+            else
+                return Color.FromRgb(255, 0, (byte)((360 - hue) * 255 / 60));
+        }
+
+
+        private async Task SearchWeatherAsync()
+        {
+            string city = cityTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(city))
+            {
+                weatherResultText.Text = "Veuillez entrer une ville valide.";
+                return;
+            }
+
+            try
+            {
+                string apiKey = File.ReadAllText("api_key.txt");
+                string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric&lang=fr";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    JObject weatherData = JObject.Parse(responseBody);
+
+                    string description = weatherData["weather"][0]["description"].ToString();
+                    string temperature = weatherData["main"]["temp"].ToString();
+                    string humidity = weatherData["main"]["humidity"].ToString();
+
+                    weatherResultText.Text = $"Météo : {description}\nTempérature : {temperature} °C\nHumidité : {humidity}%";
+                }
+            }
+            catch (Exception ex)
+            {
+                weatherResultText.Text = $"Erreur : {ex.Message}";
+            }
+        }
+    }
+}
+
+
